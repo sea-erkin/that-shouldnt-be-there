79e4417c2457c3ad4de8b6129051cac643942a04
Erkin Djindjiev <seaerkin@gmail.com>
2017-09-30 19:33:09 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 CONFIG_OS=$(gawk -F= '/^NAME/{print $2}' /etc/os-release)
 START_DIR=$(pwd)
-TARGET_GO_DIR=$HOME/go/src/github.com/sea-erkin
-TARGET_PROJECT_DIR=$TARGET_GO_DIR/that-shouldnt-be-there
+TARGET_GO_DIR=$HOME/go/src/github.com/sea-erkin/
+TARGET_PROJECT_DIR=$TARGET_GO_DIR/that-shouldnt-be-there/
 
 export GOROOT=$HOME/go
 export GOPATH=$HOME/go  
@@ -15,9 +15,7 @@ if [[ $CONFIG_OS = '"Ubuntu"' ]]; then
 
   apt install golang-go	
 
-  git clone https://github.com/sea-erkin/that-shouldnt-be-there.git
-  
-  mv that-shouldnt-be-there/ $TARGET_GO_DIR
+  cd .. && cp mv that-shouldnt-be-there/* $TARGET_GO_DIR
 
   cd $TARGET_GO_DIR/that-shouldnt-be-there && go get && go build
 
@@ -36,8 +34,8 @@ if [[ $CONFIG_SUBDOMAIN_SUBLISTER = true ]]; then
 
   apt install python-pip
 
-  mkdir $TARGET_PROJECT_DIR/other-tools
-  cd $TARGET_PROJECT_DIR/other-tools
+  mkdir $TARGET_PROJECT_DIR/other-tools/
+  cd $TARGET_PROJECT_DIR/other-tools/
  
   git clone https://github.com/aboul3la/Sublist3r.git
   
