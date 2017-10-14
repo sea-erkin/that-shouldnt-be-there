271498632bc46c1215434a8c8bc41a80f5b83ef2
erkin <seaerkin@gmail.com>
2017-10-14 12:03:11 -0400

    Initial setup script completed for mac

--- setup.sh
+++ setup.sh
@@ -1,54 +1,163 @@
+#!/bin/bash
+# Import config
 . ./config.cfm
 
-CONFIG_OS=$(gawk -F= '/^NAME/{print $2}' /etc/os-release)
-START_DIR=$(pwd)
+# Global Variables
+userid=$(id -u)
+osinfo=$(cat /etc/issue|cut -d" " -f1|head -n1)
+unameOut="$(uname -s)"
+startDir=$(pwd)
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+# Go source installation variables
 TARGET_GO_DIR=$HOME/go/src/github.com/sea-erkin
 TARGET_PROJECT_DIR=$TARGET_GO_DIR/that-shouldnt-be-there
 
+# Make go directories
 mkdir -p $HOME/goroot
 mkdir -p $TARGET_PROJECT_DIR
 
-if [[ $CONFIG_OS = '"Ubuntu"' || $CONFIG_OS = '"Kali GNU/Linux"' ]]; then
+clear
 
-  apt install golang-go	
+echo '#######################################################################'
+echo '#                       That-Shouldnt-Be-There                        #'
+echo '#######################################################################'
+echo
 
-  cd .. && cp that-shouldnt-be-there/* $TARGET_PROJECT_DIR
-
-  export GOROOT=$HOME/goroot
-  export GOPATH=$HOME/go  
-  export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
-  cd $TARGET_PROJECT_DIR && go get && go build
-
- # rm -rf $START_DIR
-
+if [ "${userid}" != '0' ]; then
+  echo '[Error]: You must run this setup script with root privileges.'
+  echo
+  exit 1
 fi
 
-if [[ $CONFIG_STORE_RESULTS = true ]]; then
+echo $osinfo
 
-  apt install sqlite3
-  cd $TARGET_PROJECT_DIR && sqlite3 ex1 ./state/tsbt.db -init ./state/initDb.sql
+case ${machine} in
+  Mac)
 
+    # install golang
+    echo
+    echo "[*] Installing golang to build source"
+    wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" https://storage.googleapis.com/golang/go1.9.1.darwin-amd64.tar.gz
+    tar -C /usr/local -xzf go1.9.1.darwin-amd64.tar.gz
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bash_profile
+
+    # install phantomjs
+    echo
+    echo "[*] Installing phantomjs binaries"
+    wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip
+    unzip phantomjs-2.1.1-macosx.zip
+    mv phantomjs-2.1.1-macosx/bin/phantomjs ./other-tools/phantomjs/
+    rm -rf phantomjs-2.1.1-macosx
+    rm phantomjs-2.1.1-macosx.zip
+    echo "[*] Installed phantomjs"
+
+    # install sqlite3
+    echo
+    echo "[*] Installing sqlite3 binaries"
+    wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" https://www.sqlite.org/2017/sqlite-tools-osx-x86-3200100.zip
+    unzip sqlite-tools-osx-x86-3200100.zip
+    mkdir ./other-tools/sqlite3
+    mv sqlite-tools-osx-x86-3200100/sqlite3 ./other-tools/sqlite3
+    rm -rf sqlite-tools-osx-x86-3200100/
+    rm sqlite-tools-osx-x86-3200100.zip
+    cd $TARGET_PROJECT_DIR
+    echo "[*] Installed sqlite3"
+
+  ;;
+  Linux)
+    # Get the flavor of linux and install golang respectively of each distro. Arch, ubuntu/kali for now
+    apt install golang-go	
+
+    cd .. && cp that-shouldnt-be-there/* $TARGET_PROJECT_DIR
+
+    export GOROOT=$HOME/goroot
+    export GOPATH=$HOME/go  
+    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+    cd $TARGET_PROJECT_DIR && go get && go build
+    ;;
+  *)
+  echo 'Machine not detected'
+esac
+
+# None machine specific tools
+# install sublister
+echo
+echo "[*] Installing Sublist3r for passive subdomain identification"
+git clone https://github.com/aboul3la/Sublist3r.git
+mv Sublist3r ./other-tools
+cd ./other-tools/Sublist3r
+sudo pip install -r requirements.txt
+cd $TARGET_PROJECT_DIR
+echo "[*] Installed Sublist3r"
+
+if [[ $CONFIG_SUBDOMAIN_ALTDNS = true ]]; then
+  # install altdns
+  echo
+  echo "[*] Installing altdns for subdomain permutation"
+  git clone https://github.com/infosec-au/altdns.git
+  mv altdns ./other-tools
+  cd ./other-tools/altdns
+  sudo pip install -r requirements.txt
+  echo "[*] Installed altdns"
+  cd $TARGET_PROJECT_DIR
 fi
 
-if [[ $CONFIG_SUBDOMAIN_SUBLISTER = true ]]; then
+# initialize sqlite database schema
+echo
+echo "[*] Initializing sqlite3 database to store results"
+./other-tools/sqlite3/sqlite3 tsbt.db tsbt.db -init ./state/initDb.sql
+mv tsbt.db ./state
+echo "[*] Initialized sqlite3 database"
 
-  apt install python-pip
-
-  mkdir $TARGET_PROJECT_DIR/other-tools/
-  cd $TARGET_PROJECT_DIR/other-tools/
- 
-  git clone https://github.com/aboul3la/Sublist3r.git
+# if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+#     wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+#     tar -xvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+#     cd phantomjs-2.1.1-linux-x86_64/bin/
+#     mv phantomjs ../../
+#     cd ../..
+#     rm -rf phantomjs-2.1.1-linux-x86_64
+#     rm phantomjs-2.1.1-linux-x86_64.tar.bz2
+# else
+#     wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36" https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2
+#     tar -xvf phantomjs-2.1.1-linux-i686.tar.bz2
+#     cd phantomjs-2.1.1-linux-i686/bin/
+#     mv phantomjs ../../
+#     cd ../..
+#     rm -rf phantomjs-2.1.1-linux-i686
+#     rm phantomjs-2.1.1-linux-i686.tar.bz2
+# fi
   
-  cd Sublist3r && pip install -r requirements.txt
+# OS Specific Installation Statement
+# case ${osinfo} in
+#   # Kali 2 dependency Install
+#   Kali2)   
+#   ;;
+#   # Kali Dependency Installation
+#   Kali)
+#   ;;
+#   # Debian 7+ Dependency Installation
+#   Debian)
+#   ;;
+#   # Ubuntu Dependency Installation
+#   Ubuntu)
+#   ;;
+#   # Notify Manual Installation Requirement And Exit
+#   *)
+#     echo "[Error]: ${osinfo} is not supported by this setup script."
+#     echo
+#     exit 1
+# esac
 
-fi
-
-if [[ $CONFIG_SCREENSHOT_RESULTS = true ]]; then
-  
-  mkdir $TARGET_PROJECT_DIR/other-tools/
-  cd $TARGET_PROJECT_DIR/other-tools/
-
-  # this will be all of the phantom js stuffs
-
-fi
+# Finish Message
+echo '[*] Setup script completed successfully.'
+echo
